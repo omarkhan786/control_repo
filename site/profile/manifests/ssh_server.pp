5658c53d6c9e6312198aea12a87ff22eb178fa1f
omarkhan786 <60778560+omarkhan786@users.noreply.github.com>
2022-04-07 17:52:15 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDQOoC16kazl2xa99Dawvlq4iOFqNyfig+/HlLjKWiENlYo/aKj+lpqEN61sZ6+yxOh5QVzh6Huay7/Jru47uXjifulNpFqpCg9NccT8/tXQUrw6gsoeFHrBrKTlr8LFENf1EQMjHZY1p7ssXPclbsiWnMx/auNnAT4PeIq3M5YOGf8FyzWAGf4vJY8SDBaQTZN4pugyP8kJqbzBMb2/iDa5sdCjoiiD8OW5/kIh/MpsuEG9XSW3NIzMRM6ab6ywKaP9AMqsI43yBm2wgIFSmMkzvtPmamMJxWpHkmKOIMINEtPxxlTLB71qKYaowQOo5DXfEhzSzzvPU7jCxr8M743',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDQOoC16kazl2xa99Dawvlq4iOFqNyfig+/HlLjKWiENlYo/aKj+lpqEN61sZ6+yxOh5QVzh6Huay7/Jru47uXjifulNpFqpCg9NccT8/tXQUrw6gsoeFHrBrKTlr8LFENf1EQMjHZY1p7ssXPclbsiWnMx/auNnAT4PeIq3M5YOGf8FyzWAGf4vJY8SDBaQTZN4pugyP8kJqbzBMb2/iDa5sdCjoiiD8OW5/kIh/MpsuEG9XSW3NIzMRM6ab6ywKaP9AMqsI43yBm2wgIFSmMkzvtPmamMJxWpHkmKOIMINEtPxxlTLB71qKYaowQOo5DXfEhzSzzvPU7jCxr8M743',
   }  
 }
